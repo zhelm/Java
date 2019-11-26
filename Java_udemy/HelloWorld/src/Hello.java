a5ffda394a10b7e08b9ff48bc32b3c5535a46b1b
Zhain <zhelm@student.wethinkcode.co.za>
2019-11-26 21:57:23 +0200

    Did some basic Java. It is better than this morning

---
+++ Java_udemy/HelloWorld/src/Hello.java
@@ -0,0 +1,16 @@
+public class Hello {
+
+    public static void main(String[] args){
+        System.out.println("Hello Bob");
+
+        int myFristNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdDumber = myFristNumber * 2;
+        int myTotal = myFristNumber + mySecondNumber + myThirdDumber;
+        System.out.println(myTotal);
+        int myLastOne = myTotal - 1000;
+        System.out.println(myLastOne);
+
+
+    }
+}
